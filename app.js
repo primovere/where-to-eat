b700cc6cfd332e451676d52aaad141e6785ebd59
primovere <p.8izo3@slmail.me>
2024-06-13 16:27:17 +0800

    build: use express-handlebars as view engine

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
 
 app.use(express.static("public"));
 
@@ -8,8 +13,8 @@ app.get("/", (req, res) => {
   res.redirect("/where-to-eat");
 });
 
-app.get("/", (req, res) => {
-  res.send("listing restaurants");
+app.get("/where-to-eat", (req, res) => {
+  res.render("index");
 });
 
 app.get("/where-to-eat/:id", (req, res) => {
