f2b6866bca4b70c98a25d05c4568bddcf9892105
primovere <p.8izo3@slmail.me>
2024-06-13 15:07:09 +0800

    build: route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("express app for restaurants");
+  res.redirect("/where-to-eat");
+});
+
+app.get("/where-to-eat", (req, res) => {
+  res.send("listing restaurants");
+});
+
+app.get("/where-to-eat/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`read restaurant: ${id}`);
 });
 
 app.listen(port, () => {
