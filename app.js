3d0352cbfc820400832cac187894f59cbd91da21
primovere <p.8izo3@slmail.me>
2024-06-25 10:20:37 +0800

    feat: add search bar and restaurant cards

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
+const restaurants = require("./public/jsons/restaurant.json").results;
 
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
@@ -10,14 +11,14 @@ app.set("views", "./views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.redirect("/where-to-eat");
+  res.redirect("/restaurants");
 });
 
-app.get("/where-to-eat", (req, res) => {
-  res.render("index");
+app.get("/restaurants", (req, res) => {
+  res.render("index", { restaurants: restaurants });
 });
 
-app.get("/where-to-eat/:id", (req, res) => {
+app.get("/restaurants/:id", (req, res) => {
   const id = req.params.id;
   res.send(`read restaurant: ${id}`);
 });
