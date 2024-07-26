46a184d1da831f0689d03cd1177df7f14fec0e9e
primovere <p.8izo3@slmail.me>
2024-07-26 12:36:21 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -18,9 +18,10 @@ app.get("/restaurants", (req, res) => {
   res.render("index", { restaurants: restaurants });
 });
 
-app.get("/restaurants/:id", (req, res) => {
+app.get("/restaurant/:id", (req, res) => {
   const id = req.params.id;
-  res.send(`read restaurant: ${id}`);
+  const restaurant = restaurants.find((rt) => rt.id.toString() === id);
+  res.render("detail", { restaurant });
 });
 
 app.listen(port, () => {
